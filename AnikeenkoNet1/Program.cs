bd07dd06707021ab4ade094aacb7ceb61e631092
Valera34 <Valeraanikeenko@gmail.com>
2018-01-28 15:49:21 +0200

    Add project files.

---
+++ AnikeenkoNet1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnikeenkoNet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Student st2 = new Student("Sasha", new Dictionary<string, int>()
+                                                            {
+                                                                {"Obdz",20},
+                                                                {"Math", 20},
+                                                                {"Ukr",20}
+                                                            });
+
+            Student st = new Student("Max", new Dictionary<string, int>()
+                                                            {
+                                                                {"Obdz",75},
+                                                                {"Math", 80},
+                                                                {"Ukr",100}
+                                                            });
+            
+            Student st3 = new Student("Leon", new Dictionary<string, int>()
+                                                            {
+                                                                {"Obdz",60},
+                                                                {"Math", 50},
+                                                                {"Ukr",70}
+                                                            });
+            
+            List<Student> students = new List<Student>();
+            students.Add(st);
+            students.Add(st2);
+            students.Add(st3);
+
+            students.Sort((s1, s2) => s2.getAverage().CompareTo(s1.getAverage()));
+            foreach (var student in students)
+            {
+                Console.Write(student.ToString() + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AnikeenkoNet1/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnikeenkoNet1
+{
+    class Student
+    {
+        public String name;
+        public Dictionary<string, int> grades;
+        public Student(string name, Dictionary<string, int> grades)
+        {
+            this.name = name;
+            this.grades = grades;
+        }
+
+        public double getAverage()
+        {
+            double average = grades.Values.Average(num => Convert.ToInt64(num));
+            return average;
+        }
+
+        public override string ToString()
+        {
+            String res = "";
+            foreach (KeyValuePair<string, int> pair in grades)
+            {
+                
+              res =res +"  "+   pair.Key.ToString() + "  -  " + pair.Value.ToString();
+            };
+            return base.ToString() + ": " + name.ToString() + " " + res +" "+ "Average mark: "+getAverage()+" \n";
+        }
+    }
+}
